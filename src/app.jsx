fc8c73403e02468129d300019fd92c0b94fd1578
Emmanuel Salcedo <emsalcedo@csu.fullerton.edu>
2022-12-17 12:19:07 -0800

    added bits from ex 4 to work with pocketbase

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+const USERNAME = 'ADMIN_EMAIL'
+const PASSWORD = 'ADMIN_PASSWORD'
+
+const pb = new PocketBase('http://127.0.0.1:8090')
+
+const authData = await pb.admins.authWithPassword(USERNAME, PASSWORD)
+console.log(authData)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
